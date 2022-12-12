70f526d564bf4d0b97267196d065f830c0b0fbc5
josue baez <josuebaez61@gmail.com>
2022-12-12 18:29:04 -0300

    Removido MatSlideToggleModule del proyecto base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -16,7 +15,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
-    MatSlideToggleModule,
     BrowserAnimationsModule,
   ],
   providers: [],
