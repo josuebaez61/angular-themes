5576ef5c13e4113f302af23937539a7d6a51ed06
josue baez <josuebaez61@gmail.com>
2022-12-13 09:36:24 -0300

    ADD, last version

--- src/app/components/theme-switcher/theme-switcher.component.ts
+++ src/app/components/theme-switcher/theme-switcher.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-theme-switcher',
@@ -8,7 +9,13 @@ import { Component } from '@angular/core';
 export class ThemeSwitcherComponent {
   isDarkThemeActive = false;
 
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
   onChange(newValue: boolean): void {
-    console.log(newValue);
+    if (newValue) {
+      this.document.body.classList.add('dark-mode');
+    } else {
+      this.document.body.classList.remove('dark-mode');
+    }
   }
 }
